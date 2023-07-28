81446fb053804764e5516104b7e8e6386d3f92fa
Tornike-Peradze <t_peradze@cu.edu.ge>
2023-07-28 17:52:13 +0400

    added js functionality(note done yet)

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-const startNumber = 9;
-const flipCard = document.querySelectorAll(".flip-card");
+const countToDate = new Date().setHours(new Date().getHours() + 24);
+let previousTimeBetweenDates;
+setInterval(() => {
+  const currentDate = new Date();
+  const timeBetweenDates = Math.ceil((countToDate - currentDate) / 1000);
+  flipAllCards(timeBetweenDates);
 
-[...flipCard.children].forEach((child) => {
-  child.textContent = startNumber;
-});
-flipCard.dataset.currentNumber = startNumber;
-flipCard.dataset.nextNumber = startNumber;
-flipCard.classList.add("flip");
+  previousTimeBetweenDates = timeBetweenDates;
+}, 250);
+
+function flipAllCards(time) {
+  const seconds = time % 60;
+  const minutes = Math.floor(time / 60) % 60;
+  const hours = Math.floor(time / 3600);
+
+  flip(document.querySelector("[data-hours-tens]"), Math.floor(hours / 10));
+  flip(document.querySelector("[data-hours-ones]"), hours % 10);
+  flip(document.querySelector("[data-minutes-tens]"), Math.floor(minutes / 10));
+  flip(document.querySelector("[data-minutes-ones]"), minutes % 10);
+  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10));
+  flip(document.querySelector("[data-seconds-ones]"), seconds % 10);
+}
